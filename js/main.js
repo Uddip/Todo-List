1b908cbb5f969293090eb8459b01f412e8c90df0
Uddip Amin <uddip.amin@gmail.com>
2020-02-07 13:44:37 -0500

    Completed:  - "delete" functionality.  - Modularity  - Cleaner and more structured output

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
 var input;
 var list = [];
 
+function printList() {
+  console.log("*******************");
+  list.forEach(function(todo, i) {
+    console.log(i + ": " + todo);
+  });
+  console.log("*******************");
+}
+
+function addTodo() {
+  var todo = prompt("Enter a new todo.");
+  list.push(todo);
+  console.log(todo + " added to list.");
+}
+
+function deleteTodo() {
+  var index = prompt("Enter index of todo to delete.");
+  console.log(list.splice(index, 1) + " removed from list.");
+}
+
 while (input != "quit") {
   input = prompt("What woud you like to do?");
 
   if (input === "new") {
-    list.push(prompt("Enter a new todo."));
-    console.log(list);
+    addTodo();
   } else if (input === "list") {
-    console.log(list);
+    printList();
+  } else if (input === "delete") {
+    deleteTodo();
   }
 }
 console.log("Ok, goodbye.");
